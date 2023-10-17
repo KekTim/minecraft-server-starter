ce3a669cca59d2131f9451406b45b680155d93cc
KekTim <TimBaumann2206@gmail.com>
2023-10-17 20:52:03 +0200

    renamed files

--- client/discordBot.py
+++ client/discordBot.py
@@ -1,3 +1,6 @@
+from time import sleep
+from subprocess import Popen 
+
 #for .env file 
 import os
 from dotenv import load_dotenv
@@ -14,7 +17,7 @@ import json
 
 intents = discord.Intents.default()
 intents.message_content = True
-client = commands.Bot(command_prefix=".", intents=intents, activity=discord.Game(name="Server Live!"), status=discord.Status.idle)
+client = commands.Bot(command_prefix=".", intents=intents, activity=discord.Game(name="Bot Online"), status=discord.Status.idle)
 
 @client.event
 async def on_ready():
@@ -24,13 +27,31 @@ async def on_ready():
 @client.tree.command(name="start", description="Start the Minecraft Server")
 async def start(ctx):
 
+    # localhost will have to be replaced with the static address of the minecraft server or sum
+    with connect("ws://localhost:8000") as websocket:
+        websocket.send(json.dumps({"type": "status"}))
+        message = websocket.recv()
+
+        if message:
+            print("on")
+        else:
+            print("off")
+
+    #check if the server is already running
+
+    await ctx.response.send_message("Starting the Minecraft Server")
+
     # start wake on lan protocol to start
     # pc with the minecraft server on it
-    await ctx.response.send_message("start")
+    await client.change_presence(activity=discord.Game(name="Starting Server"), status=discord.Status.idle)
+
+    #check through pings if the server has started
+    await client.change_presence(activity=discord.Game(name="Server is online"), status=discord.Status.online)
+    
+
 
 @client.tree.command(name="stop", description="Stop the Minecraft Server and Back it up")
 async def stop(ctx):
-
     await client.change_presence(activity=discord.Game(name="Stoping the Server"), status=discord.Status.dnd) 
 
     # call the websocket on the pc with the minecraft server running on it to close and back it up.
