f5db7fc3df65efb6ea5b3e14b31df81786390185
KekTim <TimBaumann2206@gmail.com>
2023-10-17 21:56:05 +0200

    a minecraft server now gets started with the giving path and the python script waits till the server is done starting

--- server/serverHandler.py
+++ server/serverHandler.py
@@ -2,7 +2,6 @@
 # the PC should have no password
 
 
-
 #for .env file 
 import os
 from dotenv import load_dotenv
@@ -10,30 +9,34 @@ load_dotenv(os.path.join(os.path.dirname(__file__), ".env"))
 
 #start the server!
 import subprocess
-subprocess.Popen([os.environ.get("SERVER_START_FILE")], shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
+
+def startMinecraftServer():
+    server = subprocess.Popen([os.environ.get("SERVER_START_FILE")], shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
+    while True:
+
+        # some check if this takes like 50 seconds and if yes then stop
+        # and maybe restart and keep track of how many restarts happend
+        # so when there are 3 consecutive just stop and shut down or sum
+
+        output_line = str(server.stdout.readline().strip())
+        if "Done" in output_line:
+            print("Minecraft Server has Started!")
+            return server
+server = startMinecraftServer()
 
 
 import websockets
 import asyncio
 import json
 
-async def handler(websocket):
+async def close(websocket):
     async for message in websocket:
         data = json.loads(message)
 
-        if data["type"] == "backup":
-            await websocket.send("backuped")
-            continue
-
-        # kinda redundaned because only message that can get send is the stop request.
-        # expect if i make an extra option to back the server up manually
-        if data["type"] == "stop":
-            await websocket.send("stopped") 
-            continue
 
 
 async def main():
-    async with websockets.serve(handler, "", 8000): #idk if there neds to be an ip in the ""
+    async with websockets.serve(close, "", 8000): #idk if there neds to be an ip in the ""
         await asyncio.Future()  # run forever
 
 asyncio.run(main())
