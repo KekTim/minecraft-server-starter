4268e8d685aba03044c5e2a572d04aae24963c05
KekTim <TimBaumann2206@gmail.com>
2023-10-18 16:10:41 +0200

    added more fletched out func to the server script by checking certain things etc.

--- server/serverHandler.py
+++ server/serverHandler.py
@@ -1,11 +1,19 @@
 # Skript gets autostarted when starting the PC over LAN
 # the PC should have no password
 
+#for the websocket
+import websockets
+import asyncio
+import json
 
 #for .env file 
 import os
 from dotenv import load_dotenv
 load_dotenv(os.path.join(os.path.dirname(__file__), ".env"))
+
+#for the backup
+import shutil
+import datetime
 
 #start the server!
 import subprocess
@@ -19,22 +27,53 @@ def startMinecraftServer():
         # so when there are 3 consecutive just stop and shut down or sum
 
         output_line = str(server.stdout.readline().strip())
+        print(output_line) 
         if "Done" in output_line:
             print("Minecraft Server has Started!")
             return server
+
+def closeMinecraftServer(serverToClose):
+    serverToClose.stdin.write(b"stop\n")
+    serverToClose.stdin.flush()
+    serverToClose.wait()
+    
+def backupMinecraftServer():
+    from werkzeug.utils import secure_filename
+    serverLocation = os.environ.get("SERVER_LOCATION")
+    backupLocation = os.path.join(os.environ.get("BACKUP_LOCATION"), secure_filename(datetime.datetime.now().strftime("Date %d.%m.%Y Time %H.%M.%S")))
+    shutil.make_archive(backupLocation, "zip", serverLocation)
+
+
 server = startMinecraftServer()
-
-
-import websockets
-import asyncio
-import json
-
+stopping = False
 async def close(websocket):
     async for message in websocket:
         data = json.loads(message)
-        server.stdin.write(b"stop\n")
-        server.stdin.flush()
-        print("Minecraft Server Stopped")
+
+        if data["type"] != "stop":
+            websocket.send(json.dumps({"message": "not right type"}))
+            websocket.close()
+            continue
+
+        if not stopping:
+            websocket.send(json.dumps({"message": "already stopping"}))
+            websocket.close()
+            continue
+
+        stopping = True
+
+        await websocket.send(json.dumps({"message": "closing server"}))
+        closeMinecraftServer(server)
+
+        await websocket.send(json.dumps({"message": "server closed and starting backup"}))
+        backupMinecraftServer()
+
+        await websocket.send(json.dumps({"message": "backup complete and starting shutdown"}))
+        websocket.close()
+        
+        import sys
+        sys.exit()
+        # os.system("shutdown /s /t 10")
 
 
 async def main():
---
+++ server/test.py
@@ -0,0 +1,18 @@
+import time
+import datetime
+
+print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+
+# import asyncio
+# import time
+
+# def test():
+#     time.sleep(5)
+#     print("test")
+
+# async def main():
+#     print("main1")
+#     test()
+#     print("main1")
+    
+# asyncio.run(main())
